3c2f0a47c2619285eb336e2b76dd6b83a8bb1188
guozhq <shxguozhq@qq.com>
2023-03-07 14:22:10 +0800

    实现手机页面路由切换后，侧边栏隐藏

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,19 @@
 </template>
 <script lang="ts">
 import {provide, ref} from 'vue';
+import {router} from './router';
 
 export default{
   name:'App',
   setup(){
     const width = document.documentElement.clientWidth;
     const asideVisible = ref(width > 500)
-    console.log(asideVisible)
     provide('asideVisible', asideVisible)
+    router.afterEach(()=>{
+      if (width <= 500){
+        asideVisible.value = false;
+    }
+    });
   }
 }
 </script>
