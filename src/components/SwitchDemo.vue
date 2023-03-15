0fe37dd0b0ef3bccb640f3bebc392820be23343a
guozhq <shxguozhq@qq.com>
2023-03-15 22:23:20 +0800

    Switch第一版：实现按钮点击切换位置。

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,10 +1,11 @@
 <template>
-  SwitchDemo
+  <Switch />
 </template>
 
 <script lang="ts">
+import Switch from '../lib/Switch.vue'
 export default {
-  name: 'SwitchDemo'
+  components: { Switch },
 };
 </script>
 
---
+++ src/lib/Switch.vue
@@ -0,0 +1,45 @@
+<template>
+  <button class="pika-switch" :class="{checked}" @click="toggle">
+    <span class="pika-switch-handle"></span>
+  </button>
+</template>
+<script lang="ts">
+import {ref} from 'vue';
+export default{
+  setup(){
+    const checked=ref(false);
+    const toggle=()=>{
+      checked.value = !checked.value
+    }
+    return {checked, toggle}
+  }
+}
+</script>
+<style lang="scss">
+$h: 22px;
+$h2: $h - 4px;
+.pika-switch{
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: gray;
+  border-radius: calc($h/2);
+  position: relative;
+  > span{
+    position: absolute;
+    width: $h2;
+    height: $h2;
+    background: white;
+    top: 2px;
+    left: 2px;
+    border-radius: calc($h2/2);
+    transition: left 250ms;
+  }
+  &.checked{
+    background: blue;
+    > span{
+      left: calc(100% - $h2 - 2px);
+    }
+  }
+}
+</style>
