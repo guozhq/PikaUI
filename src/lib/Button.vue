ef31d1236d1af158d1de1be8861f75bd887364c4
guozhq <shxguozhq@qq.com>
2023-03-16 21:05:36 +0800

    完成Button基本样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -14,9 +14,30 @@ export default {
 }
 </script>
 <style lang="scss">
+$h:32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
 .pika-button{
   box-sizing: border-box;
-  height: 32px;
+  height: $h;
   padding: 0 12px;
+  cursor: pointer;
+  border-radius: $radius;
+  border: 1px solid $border-color;
+  background: white;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus{
+    outline: none;
+  }
 }
 </style>
