45210d772ca5dfa8ce71070501c8cd92b9e4269e
guozhq <shxguozhq@qq.com>
2023-03-17 00:03:51 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,12 @@
 <template>
-  DialogDemo
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
 <script lang="ts">
+import Dialog from "../lib/Dialog.vue"
 export default {
-  name: 'DialogDemo'
+  components: {Dialog}
 };
 </script>
 
