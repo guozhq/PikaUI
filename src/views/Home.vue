0f4ca4a03fd927dd23ba6e8fde3f81fd4f00de04
guozhq <shxguozhq@qq.com>
2023-03-18 12:40:54 +0800

    使用 clip-path 画圆弧

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,6 +38,7 @@ $color: #28d1c9;
 .topnavAndBanner{
   background: rgb(227,255,253);
   background: linear-gradient(145deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
 .features{
   >svg{
